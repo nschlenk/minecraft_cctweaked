0a1292f454e2e20cc13f10aa410f59bb3c8a8165
NOELLE SCHLENK <noelle.schlenk@gmail.com>
2025-03-30 12:19:17 -0700

    Added additional commands

--- main.lua
+++ main.lua
@@ -1,7 +1,15 @@
 -- Noelle is trying her best
+-- ~= means not equal unless it's boolean, then it's "not"
+-- boolean values are all lowercase
+-- variables are global by default
+-- nil instead of null
+
 
 function Main()
   turtle.forward()
+  turtle.turnLeft()
+  turtle.foward()
+  print("oopsie")
 end
 
 Main()
