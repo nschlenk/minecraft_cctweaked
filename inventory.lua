c1d08c9f396086059d77389c19e88ab487cb7258
nschlenk <noelle.schlenk@gmail.com>
2025-04-16 15:30:49 -0700

    Update inventory.lua

--- inventory.lua
+++ inventory.lua
@@ -1,11 +1,18 @@
 
 input = peripheral.find("minecraft:chest")
-monitor = peripheral.find("monitor")
+local m = peripheral.wrap("top")
+v = 1
 while true do
   item = input.getItemDetail(1)
-  if item ~= nil then
-    monitor.clear()
-    monitor.write(item.name)
-    print(item.name)
+  if item ~= nil and item ~= v then
+    m.clear()
+    m.setCursorPos(1, 1)
+    n = item.name
+    m.write(n)
+    v = item
+  elseif item == nil then
+    m.clear()
+    m.setCursosPos(1, 1)
+    m.write("None")
   end
 end
