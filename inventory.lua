71d56cff8b77ee6cc9f872fab96c26f5bbe6bccb
nschlenk <noelle.schlenk@gmail.com>
2025-04-15 19:37:32 -0700

    Update inventory.lua

--- inventory.lua
+++ inventory.lua
@@ -1,6 +1,8 @@
 
 input = peripheral.find("minecraft:chest")
+monitor = peripheral.find("monitor")
 while true do
   item = input.getItemDetail(1)
-  peripheral.call("top", "write", item.name)
+  monitor.clear()
+  monitor.write(item.name)
 end
